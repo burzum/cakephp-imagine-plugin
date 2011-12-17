720385a4bb898b4967b3bdcf7111432f68bf1c0b
Florian Krämer <florian.kraemer@cakedc.com>
2011-12-17 17:26:36 +0100

    Fixing a lower cased folder name in the inclusion path

--- Lib/ImagineLoader.php
+++ Lib/ImagineLoader.php
@@ -5,7 +5,7 @@
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright Copyright 2010, Cake Development Corporation (http://cakedc.com)
+ * @copyright Copyright 2011, Cake Development Corporation (http://cakedc.com)
  * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 namespace Imagine;
@@ -26,7 +26,7 @@ class ImagineLoader {
 	public static function load($name) {
 		$imagineBase = \Configure::read('Imagine.base');
 		if (empty($imagineBase)) {
-			$imagineBase = \CakePlugin::path('Imagine') . 'Vendor' . DS . 'Imagine' . DS . 'lib' . DS;
+			$imagineBase = \CakePlugin::path('Imagine') . 'Vendor' . DS . 'Imagine' . DS . 'Lib' . DS;
 		}
 
 		$filePath = $imagineBase . $name . '.php';

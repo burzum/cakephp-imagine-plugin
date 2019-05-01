b76d9b0e4c289d33940023906e70ad3e11b2b81c
Yevgeny Tomenko <skie@mail.ru>
2019-05-01 09:08:19 +0300

    added plugin

---
+++ src/Plugin.php
@@ -0,0 +1,14 @@
+<?php
+namespace Burzum\Imagine;
+
+use Cake\Core\BasePlugin;
+
+class Plugin extends BasePlugin
+{
+    /**
+     * Plugin name.
+     *
+     * @var string
+     */
+    protected $name = 'Burzum/Imagine';
+}
